82435007b02bcfe8df83b48b6c0b6a7fe6b7956c
Alawode Samuel <tolulope.alawode@gmail.com>
2023-07-02 08:49:11 +0100

    new commits

---
+++ Spedire/src/main/java/com/spedire/Spedire/SpedireApplication.java
@@ -0,0 +1,13 @@
+package com.spedire.Spedire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpedireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpedireApplication.class, args);
+	}
+
+}
