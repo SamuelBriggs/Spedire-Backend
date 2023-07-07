b5e6c4914aa54a73a06755ff0df7f04bc0e0b612
Michael Josh <michaelikejoshua@gmail.com>
2023-07-07 02:50:27 +0100

    Email verification function added

--- Spedire/src/main/java/com/spedire/Spedire/services/BrevoEmailService.java
+++ Spedire/src/main/java/com/spedire/Spedire/services/BrevoEmailService.java
@@ -1,25 +1,16 @@
 package com.spedire.Spedire.services;
 
 import com.spedire.Spedire.configurations.EmailConfig;
-import com.spedire.Spedire.dtos.request.Recipient;
 import com.spedire.Spedire.dtos.request.SendEmailRequest;
-import com.spedire.Spedire.dtos.request.Sender;
 import com.spedire.Spedire.dtos.response.SendEmailResponse;
-import com.spedire.Spedire.dtos.templates.VerifyEmailTemplate;
-import com.spedire.Spedire.exceptions.SpedireException;
-import com.spedire.Spedire.models.User;
-import com.spedire.Spedire.utils.JwtUtil;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.Set;
 
-import static com.spedire.Spedire.services.TokenService.generateToken;
 import static com.spedire.Spedire.utils.Constants.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
--- Spedire/src/main/java/com/spedire/Spedire/utils/Constants.java
+++ Spedire/src/main/java/com/spedire/Spedire/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String APP_NAME="Spedire Tech";
     public static final String APP_EMAIL="noreply@spediretech";
     public static final String ACTIVATION_LINK_VALUE="Activation Link";
-    public static final String FRONTEND_BASE_URL = "https://www.spedire.com";
+    public static final String FRONTEND_BASE_URL = "http://localhost:3001/verified";
     public static final String ID = "id";
     public static final String EMAIL_URL="https://api.brevo.com/v3/smtp/email";
     public static final String API_KEY_VALUE = "api-key";
