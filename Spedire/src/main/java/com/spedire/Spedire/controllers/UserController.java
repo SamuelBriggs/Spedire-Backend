e63c84f3bde6a58d5c9c5e1514b86d74187c1951
Michael Josh <michaelikejoshua@gmail.com>
2023-07-07 14:15:16 +0100

    New

--- Spedire/src/main/java/com/spedire/Spedire/controllers/UserController.java
+++ Spedire/src/main/java/com/spedire/Spedire/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3001", methods = {RequestMethod.GET, RequestMethod.POST})
+//@CrossOrigin(origins = "http://localhost:3001", methods = {RequestMethod.GET, RequestMethod.POST})
 @RestController
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
